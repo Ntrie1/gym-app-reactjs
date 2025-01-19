13d0fdd0f3d83d909204299d07ca92c3edb33b1c
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2025-01-19 23:25:02 +0200

    description added

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -28,6 +28,16 @@ export default function ExerciseCard({ exercise, index }) {
                 <p className='capitalize'>{exercise.muscles.join(' & ')}</p>
             </div>
 
+            <div className='flex flex-col bg-slate-950 rounded gap-2'>
+                {exercise.description.split('___').map((val) => {
+                    return (
+                        <div className='text-sm '>
+                            <p>{val}</p>
+                        </div>
+                    )
+                })}
+            </div>
+
             <div className='grid grid-cols-2 sm:grid-cols-4 sm:place-items-center gap-2'>
                 {['reps', 'rest', 'tempo'].map(info => {
                     return (
