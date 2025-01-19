fd7f8d3e9e0c2c03bc0191ee41bf18430bfda5a6
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2025-01-19 23:13:07 +0200

    exercise card added

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import SectionWrapper from './SectionWrapper'
+import ExerciseCard from './ExerciseCard'
 
-export default function Workout({workout}) {
+export default function Workout({ workout }) {
   return (
-    <div>Workout</div>
+    <SectionWrapper
+      header={"Welcome to"}
+      title={['The', 'DAMGER', 'zone']}>
+      <div className='flex flex-col gap-4'>
+        {workout.map((exercise, i) => {
+         return (
+           <ExerciseCard 
+           exercise={exercise}
+           index={i}
+           key={i}
+           />
+          )
+        })}
+      </div>
+    </SectionWrapper>
   )
 }
